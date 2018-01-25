fbde72b627cacad2b5c00881df46e19a424cb284
Yash Raikar <yashraikar1087@gmail.com>
2018-01-24 20:45:07 -0700

    Initial Comit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials/');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+app.use((req,res,next)=>{
+    res.render('maintence.hbs',{
+        pageTitle:'maintainence'
+    });
+    //next();
+});
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage:'Welcome Home'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+    });
+});
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:"could not load"
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("server is up on port 3000");
+});
