b2a7242454ff3adbf53a9184a10743b62bd28460
Yash Raikar <yashraikar1087@gmail.com>
2018-01-24 21:54:20 -0700

    removed maintainence

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use((req,res,next)=>{
-    res.render('maintence.hbs',{
-        pageTitle:'maintainence'
-    });
+// app.use((req,res,next)=>{
+//     res.render('maintence.hbs',{
+//         pageTitle:'maintainence'
+//     });
     //next();
 });
 
